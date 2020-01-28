014035ab99316c2ba572c9203a036c209a34f93b
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-28 18:48:04 +0000

    Counter variable reflects in real time the amount of characters left & turns red when it goes into the negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,16 @@
-//This ensures that the DOM is loaded
+//This ensures that the DOM has loaded
+
+$(document).ready(function() {
+  // let counter = 0;
+  const maxLength = 140;
+
+  $('textarea').keyup(function() {
+    let currentChars = maxLength - $(this).val().length;
+    $('.counter').text(currentChars);
+    if($(this).val().length > maxLength) {
+      $('.counter').text(currentChars).css('color','red');
+    }
+
+  })
+  
+})
