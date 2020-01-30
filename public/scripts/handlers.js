e45297cccc006339c00ed4a38db93661815d7b4c
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-30 00:34:54 +0000

    Refactored code, made it more modular & added comments

---
+++ public/scripts/handlers.js
@@ -0,0 +1,44 @@
+//This file handles the POST and GET requests 
+
+///////// POST REQUEST ///////////////
+const postTweets = () => {
+  $('form').submit(function(event) {
+  event.preventDefault();
+  const serializedData =  $(this).serialize();
+  
+//Here we are checking form validation
+  if(String($(this).find('textarea').val()).length === 0) {
+
+    $('#noChars').slideToggle();
+
+  }
+     else if (String($(this).find('textarea').val()).length <= 140) {
+
+      $.ajax('/tweets', {method: 'POST', data: `${serializedData}`})
+      .then(function() {
+        loadTweets();
+      })
+      
+    } 
+    else {
+      
+        $('#above140').slideToggle();
+
+      };
+      
+      
+    }
+  
+  )
+}
+
+
+////GET REQUEST /////////////////
+const loadTweets = () => {
+  $.ajax('/tweets', {method: 'GET'})
+  .then(function(data){
+    console.log('fetched data: ', data);
+    renderTweets(data);
+  }) 
+  
+}
---
+++ public/scripts/tweetCreatingAndRendering.js
@@ -0,0 +1,44 @@
+/////THIS FUNCTION ITERATES THROUGH THE TWEET OBJECTS AND RENDERS THEM TO THE HTML ///////
+
+const renderTweets = (tweets) => {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  
+  for(const tweet of tweets) {
+    $('.tweetContainer').prepend(createTweetElement(tweet)); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  }
+  // takes return value and appends it to the tweets container
+  }
+
+
+  ////THIS FUNCTION CREATES A MARKUP TEMPLATE FOR THE TWEET OBJECT/////////
+
+  const createTweetElement = (tweet) => {
+
+    let $tweet = $(`<article class='prevTweets'>
+             <header id='prev-tweets-header'>  
+               <div class='leftElementsContainer'>
+                 <img class='profileImg' src=${escape(tweet.user.avatars)}>
+                 <p class='nameTweet'>${escape(tweet.user.name)}</p>
+   
+               </div>
+   
+                 <h4>${escape(tweet.user.handle)}</h4>
+             </header>
+             <div class='tweetBody'>
+               <p>${escape(tweet.content.text)}</p>
+             </div>
+             <footer>
+               <p id='timeLength'>${escape(date(tweet.created_at))} days ago</p>
+               <div class='icons'>
+                 <i class="fa fa-flag"></i>
+                 <i class="fa fa-retweet"></i>
+                 <i class="fa fa-heart"></i>
+               </div>
+             </footer>
+           </article>
+    `).addClass('tweet');
+    
+   return $tweet;
+   }
+   
